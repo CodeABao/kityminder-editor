b01278d4ecb3834da16ebf0e1e021b822299087b
codeabao <libaoxin418@126.com>
2021-11-09 13:52:22 +0800

    临时版本

--- dist/kityminder.editor.min.js
+++ dist/kityminder.editor.min.js
@@ -1,6 +1,6 @@
 /*!
  * ====================================================
- * kityminder-editor - v1.0.67 - 2021-11-08
+ * kityminder-editor - v1.0.67 - 2021-11-09
  * https://github.com/fex-team/kityminder-editor
  * GitHub: https://github.com/fex-team/kityminder-editor 
  * Copyright (c) 2021 ; Licensed 
